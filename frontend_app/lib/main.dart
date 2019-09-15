2cf501e224b4f9059bf0f7bcd7eb0b10a59ed429
Timothy Sealy <timothy.sealy@gmail.com>
2019-09-15 20:48:40 +0200

    Added netmobiel icons.

--- frontend_app/lib/main.dart
+++ frontend_app/lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 String url = "https://app.netmobiel.eu";
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarColor: Colors.white, //top bar color
-        statusBarIconBrightness: Brightness.dark, //top bar icons
-        systemNavigationBarColor: Colors.white, //bottom bar color
-        systemNavigationBarIconBrightness: Brightness.dark, //bottom bar icons
-      )
-  );
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
       .then((_) => runApp(new MyApp()));
@@ -22,6 +15,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarColor(Color.fromRGBO(51,137,150, 1.0));
+    FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
+    FlutterStatusbarcolor.setNavigationBarWhiteForeground(true);
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
@@ -60,18 +56,17 @@ class _HomeState extends State<Home> {
       ),
     );
     return
-      Scaffold(
-          appBar: new AppBar(
-            backgroundColor: Colors.white,
-          ),
-          body:
+      SafeArea(
+        minimum: const EdgeInsets.all(0.0),
+        child:
           WebviewScaffold(
             userAgent: 'Flutter',
             url: url,
             withJavascript: true,
             withLocalStorage: true,
             withZoom: false,
-          )
+            scrollBar: false,
+          ),
       );
   }
 }
