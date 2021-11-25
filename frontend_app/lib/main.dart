9a8e6677d716c97bfb3b02b2c73292a02f72a09f
Jaap Reitsma <jaap.reitsma@vidinexus.com>
2021-11-25 18:22:10 +0100

    First step to updated flutter app (for Android).

--- frontend_app/lib/main.dart
+++ frontend_app/lib/main.dart
@@ -1,30 +1,61 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+// import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:device_info/device_info.dart';
 
 String url = "https://app.netmobiel.eu";
 
-void main() {
-  runApp(new MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  print("Firebase.initializeApp");
+  await Firebase.initializeApp(options: const FirebaseOptions(
+      apiKey: "AIzaSyDsvRU4TKWO-dHYkmZYowm3ptD2y7Szojc",      // Auth / General Use
+      appId: "1:119510705158:android:7c4a4cbb2b10a9c4688f87", // General Use
+      projectId: "netmobiel-push",                            // General Use
+      // authDomain: "YOUR_APP.firebaseapp.com",              // Auth with popup/redirect
+      databaseURL: "https://netmobiel-push.firebaseio.com",   // Realtime Database
+      storageBucket: "netmobiel-push.appspot.com",            // Storage
+      messagingSenderId: "",                         // Cloud Messaging
+      // measurementId: "G-12345",                               // Analytics
+      androidClientId: "119510705158-slk85pna9hhqf19481a4afsipprpr5ua.apps.googleusercontent.com",
+      iosClientId: "119510705158-auh7kbc1uot14hsd1q0qek4daa8h4v8n.apps.googleusercontent.com",
+      iosBundleId: "eu.netmobiel.frontendApp",
+      // appGroupId: "",
+    )
+  );
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  print("FirebaseMessaging.instance.requestPermission");
+  NotificationSettings settings = await FirebaseMessaging.instance
+      .requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    FlutterStatusbarcolor.setStatusBarColor(Color.fromRGBO(51, 137, 150, 1.0));
-    FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
-    FlutterStatusbarcolor.setNavigationBarWhiteForeground(true);
-    return new MaterialApp(
+    // FlutterStatusbarcolor.setStatusBarColor(Color.fromRGBO(51, 137, 150, 1.0));
+    // FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
+    // FlutterStatusbarcolor.setNavigationBarWhiteForeground(true);
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: new ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       routes: {
@@ -35,27 +66,89 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
-  _HomeState createState() => new _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-  final flutterWebviewPlugin = new FlutterWebviewPlugin();
-  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  final flutterWebviewPlugin = FlutterWebviewPlugin();
   String _fcmToken = "";
-  String _baseUrl = "https://app.netmobiel.eu";
-  String _url = "";
-  StreamSubscription<WebViewStateChanged> _onStateChanged;
+  final String _baseUrl = "https://app.netmobiel.eu";
+  late StreamSubscription<WebViewStateChanged> _onStateChanged;
   final telephonePrefix = 'tel:';
-  bool _useAcceptance = false;
   String _userAgent = 'Flutter,';
+
+  Future<void> setupInteractedMessage() async {
+    // Get any messages which caused the application to open from
+    // a terminated state.
+    print("setupInteractedMessage");
+    try {
+      RemoteMessage? initialMessage =
+      await FirebaseMessaging.instance.getInitialMessage();
+
+      // If the message also contains a data property with a "type" of "chat",
+      // navigate to a chat screen
+      print("Got initial message");
+      if (initialMessage != null) {
+        handleInitialMessage(initialMessage);
+      }
+
+      // Also handle any interaction when the app is in the background via a
+      // Stream listener
+      FirebaseMessaging.onMessageOpenedApp.listen(handleInitialMessage);
+      print("Setup listener initial message");
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    firebaseCloudMessaging_Listeners();
+    try {
+      setupInteractedMessage().then((value) =>
+          print("Interacted message setup done"));
+      FirebaseMessaging.instance.getToken()
+          .then((token) => saveToken(token))
+          .catchError((error) => print(error.toString()));
+      // Any time the token refreshes, store this in the database too.
+      FirebaseMessaging.instance.onTokenRefresh.listen(saveToken);
+      print("Setup token listener");
+      FirebaseMessaging.onMessage.listen(handleForegroundMessage);
+      print("Setup foreground listener");
+    } catch (e) {
+      print(e);
+    }
     buildUserAgentString();
+  }
+
+  void saveToken(String? token) {
+    print("Save token: ${token}");
+    token ??= "";
+    setState(() {
+      _fcmToken = token!;
+    });
+  }
+
+  void handleInitialMessage(RemoteMessage message) {
+    // Should do some navigation here
+    if (message.data != null) {
+      print('MSG Data: ${message.data}');
+    }
+    if (message.notification != null) {
+      print('MSG Notification: ${message.notification}');
+    }
+  }
+
+  void handleForegroundMessage(RemoteMessage message) {
+    if (message.data != null) {
+      print('MSG FG Data: ${message.data}');
+    }
+    if (message.notification != null) {
+      print('MSG FG Notification: ${message.notification}');
+    }
   }
 
   void buildUserAgentString() async {
@@ -71,8 +164,7 @@ class _HomeState extends State<Home> {
             'Flutter - Android $release (SDK $sdkInt), $manufacturer $model';
       });
       // Android 9 (SDK 28), Xiaomi Redmi Note 7
-    }
-    if (Platform.isIOS) {
+    } else if (Platform.isIOS) {
       var iosInfo = await DeviceInfoPlugin().iosInfo;
       var systemName = iosInfo.systemName;
       var version = iosInfo.systemVersion;
@@ -86,49 +178,18 @@ class _HomeState extends State<Home> {
     }
   }
 
-  void firebaseCloudMessaging_Listeners() {
-    if (Platform.isIOS) iOS_Permission();
-
-    _firebaseMessaging.getToken().then((token) {
-      print(token);
-      setState(() {
-        _url = "$_baseUrl?fcm=$token";
-        _fcmToken = token;
-      });
-    });
-
-    _firebaseMessaging.configure(
-      onMessage: (Map<String, dynamic> message) async {
-        print('on message $message');
-      },
-      onResume: (Map<String, dynamic> message) async {
-        print('on resume $message');
-      },
-      onLaunch: (Map<String, dynamic> message) async {
-        print('on launch $message');
-      },
-    );
-  }
-
-  void iOS_Permission() {
-    _firebaseMessaging.requestNotificationPermissions(
-        IosNotificationSettings(sound: true, badge: true, alert: true));
-    _firebaseMessaging.onIosSettingsRegistered
-        .listen((IosNotificationSettings settings) {
-      print("Settings registered: $settings");
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    print("Build the widget: Resize webview");
     flutterWebviewPlugin.resize(
-      new Rect.fromLTWH(
+      Rect.fromLTWH(
         0.0,
         0.0,
         MediaQuery.of(context).size.width,
         MediaQuery.of(context).size.height,
       ),
     );
+    print("Listen to state changes");
     _onStateChanged = flutterWebviewPlugin.onStateChanged
         .listen((WebViewStateChanged state) async {
       if (mounted) {
@@ -140,29 +201,12 @@ class _HomeState extends State<Home> {
         }
       }
     });
-    _prefs.then((SharedPreferences prefs) {
-      var tmp = prefs.getBool('enabled_acceptance') ?? false;
-      print('ACC: $tmp');
-      if (tmp != _useAcceptance) {
-        setState(() {
-          _useAcceptance = tmp;
-          _baseUrl = _useAcceptance
-              ? "https://app.acc.netmobiel.eu"
-              : "https://app.netmobiel.eu";
-          _url = _fcmToken.isNotEmpty ? "$_baseUrl?fcm=$_fcmToken" : _baseUrl;
-        });
-      }
-    });
-    if (_useAcceptance) {
-      FlutterStatusbarcolor.setStatusBarColor(Color.fromRGBO(255, 133, 0, 1.0));
-    } else {
-      FlutterStatusbarcolor.setStatusBarColor(
-          Color.fromRGBO(51, 137, 150, 1.0));
-    }
+    print("Return the safe area");
+    final String _url = _fcmToken.isEmpty ? _baseUrl : "$_baseUrl?fcm=$_fcmToken";
     return SafeArea(
       minimum: const EdgeInsets.all(0.0),
       child: _url == ""
-          ? Container(color: Color.fromRGBO(51, 137, 150, 1.0))
+          ? Container(color: const Color.fromRGBO(51, 137, 150, 1.0))
           : WebviewScaffold(
               userAgent: _userAgent,
               url: _url,
